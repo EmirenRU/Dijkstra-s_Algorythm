1d216d7ea02854d1d8e3630895fd5e5992381a0f
Emil Samigullin <mrjava3300@mail.ru>
2022-10-05 19:38:48 +0300

    0.2

--- src/Graph.java
+++ src/Graph.java
@@ -1,35 +1,26 @@
 
-import java.util.Iterator;
-import java.util.Set;
-import java.util.Vector;
+import java.util.*;
 
 public class Graph {
     static private int V;
-    static private Vector<Vector<Integer>> adjVec;
-    static private Set<Set<Integer>> adjSet;
+    static private ArrayList<ArrayList<Integer>> adj;
+    static private Set<Integer> set;
 
-    public Graph(int V){ this.V = V; adjVec = new Vector<>(this.V) ; for (var i : adjVec) { i = new Vector<>(this.V); }}
+    public Graph(int v){ V = v; adj = new ArrayList<>(v-1);}
 
     public static void setV(int v) {
         V = v;
     }
-
-    public static void setNode (int V, int VV){
-
-        // O(n^2)
-        Iterator<Set<Integer>> iter = adjSet.stream().iterator();
-        int i = 0;
-        while ( i++ != V ){
-            iter.next();
-
-
-        }
-    }
-
     public static int getV() {
         return V;
     }
 
-    public void Dijkstra(){}
+    public void addNodes(ArrayList<Integer> arr){
+        adj.add(arr);
+    }
+
+//    public Set<Integer> Dijkstra(){
+//
+//    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,67 @@
-
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
+    //static Scanner s = new Scanner(System.in);
+    static Graph graph;
+    static ArrayList<Integer> arr;
     public static void main(String[] args){
+        // 0 1 2 3 4
+        // A B C D E
+        arr = new ArrayList<>(5);
+        graph = new Graph(5);
 
+        // A
+        arr.add(0);
+        arr.add(50);
+        arr.add(0);
+        arr.add(0);
+        arr.add(90);
+
+        graph.addNodes(arr);
+        arr.clear();
+
+        // B
+        arr.add(50);
+        arr.add(0);
+        arr.add(90);
+        arr.add(0);
+        arr.add(0);
+        graph.addNodes(arr);
+        arr.clear();
+
+        // C
+        arr.add(0);
+        arr.add(90);
+        arr.add(0);
+        arr.add(80);
+        arr.add(60);
+        graph.addNodes(arr);
+        arr.clear();
+
+        // D
+        arr.add(0);
+        arr.add(0);
+        arr.add(80);
+        arr.add(0);
+        arr.add(70);
+        graph.addNodes(arr);
+        arr.clear();
+
+        // E
+        arr.add(90);
+        arr.add(0);
+        arr.add(60);
+        arr.add(70);
+        arr.add(0);
+        graph.addNodes(arr);
+
+//        Set<Integer> res = graph.Dijkstra();
+
+    }
+
+    protected void finalize(){
+        // s.close();
     }
 }
