78621fa94af1a095fd2f98b88771ed221135e64f
Emil Samigullin <mrjava3300@mail.ru>
2022-10-08 18:23:46 +0300

    1.0

--- src/Graph.java
+++ src/Graph.java
@@ -1,26 +1,73 @@
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class Graph {
     static private int V;
-    static private ArrayList<ArrayList<Integer>> adj;
-    static private Set<Integer> set;
+    static private ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-    public Graph(int v){ V = v; adj = new ArrayList<>(v-1);}
-
-    public static void setV(int v) {
-        V = v;
-    }
-    public static int getV() {
-        return V;
-    }
-
+    public Graph(int v){ V = v;}
     public void addNodes(ArrayList<Integer> arr){
         adj.add(arr);
     }
+    public static void setV(int v) { V = v; }
+    public static int getV() { return V; }
 
-//    public Set<Integer> Dijkstra(){
-//
-//    }
+    public static ArrayList<ArrayList<Integer>> getAdj() { return adj; }
+    public void ShortestPath(int src) {
+        ArrayList<Integer> dist = new ArrayList<>();
+        Boolean[] sptSet = new Boolean[V];
+
+        for (int i = 0; i < V; i++){
+            dist.add(Integer.MAX_VALUE);
+            sptSet[i] = false;
+        }
+
+        dist.set(src, 0);
+
+        for (int count = 0; count < V - 1; count++ ) {
+
+            int index = minDistance(dist, sptSet);
+
+            sptSet[index] = true;
+
+            for (int v = 0; v < V; v++) {
+
+                if (!sptSet[v] && (adj.get(index).get(v) != 0) && (dist.get(index) != Integer.MAX_VALUE)
+                        && (dist.get(index) + adj.get(index).get(v) < dist.get(v))){
+                    dist.set(v, dist.get(index) + adj.get(index).get(v));
+                }
+
+            }
+        }
+
+        printResult(dist, src);
+
+    }
+
+    private int minDistance(ArrayList<Integer> dist, Boolean[] sptSet){
+
+        int min = Integer.MAX_VALUE, min_index = -1;
+
+        for (int i = 0; i < V; i++){
+            if (!sptSet[i] && dist.get(i) <= min){
+                min = dist.get(i);
+                min_index = i;
+            }
+        }
+
+        return min_index;
+    }
+
+    private void printResult(ArrayList<Integer> dist, int src){
+        System.out.println("\nShortest path from " + ++src + " to ");
+
+        for (int i = 0; i < V; i++){
+            System.out.println(i+1 + "\t\t" + dist.get(i) + "\t\t");
+        }
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,99 +1,14 @@
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Set;
+
 
 public class Main {
-    //static Scanner s = new Scanner(System.in);
-    static Graph graph;
-    static ArrayList<Integer> arr;
-    public static void main(String[] args){
 
-        arr = new ArrayList<>(16);
-        graph = new Graph(16);
+    public static void main(String[] args) throws InterruptedException {
 
-        // v1
-        arr = makeList(new int[]{0,  3,  0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0});
-        graph.addNodes(arr);
+        Dijkstra dj = new Dijkstra();
+        dj.run();
 
-        // v2
-        arr = makeList(new int[]{0, 0, 10, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
-        graph.addNodes(arr);
-
-        // v3
-        arr = makeList(new int[]{0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0});
-        graph.addNodes(arr);
-
-        // v4
-        arr = makeList(new int[]{0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
-        graph.addNodes(arr);
-
-        // v5
-        arr = makeList(new int[]{0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3});
-        graph.addNodes(arr);
-
-        // v6
-        arr = makeList(new int[]{0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0});
-        graph.addNodes(arr);
-
-        // v7
-        arr = makeList(new int[]{0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0});
-        graph.addNodes(arr);
-
-        // v8
-        arr = makeList(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 7, 0, 0, 0});
-        graph.addNodes(arr);
-
-        // v9
-        arr = makeList(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0});
-        graph.addNodes(arr);
-
-        // v10
-        arr = makeList(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
-        graph.addNodes(arr);
-
-        // v11
-        arr = makeList(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 9});
-        graph.addNodes(arr);
-
-        // v12
-        arr = makeList(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0});
-        graph.addNodes(arr);
-
-        // v13
-        arr = makeList(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
-        graph.addNodes(arr);
-
-        // v14
-        arr = makeList(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 1, 0});
-        graph.addNodes(arr);
-
-        // v15
-        arr = makeList(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0});
-        graph.addNodes(arr);
-
-        // v16
-        arr = makeList(new int[]{0, 0, 0, 0, 0, 0, 7, 2, 3, 0, 0, 0, 0, 0, 0, 0});
-        graph.addNodes(arr);
-
-
-//        Set<Integer> res = graph.Dijkstra();
-
-    }
-
-    public static ArrayList<Integer> makeList(int[] arr){
-        ArrayList<Integer> arrayList = new ArrayList<>(16);
-
-        Arrays.stream(arr).forEach(arrayList::add);
-
-        Arrays.stream(arr).forEach(i -> i = 0);
-
-        return arrayList;
-    }
-
-
-    protected void finalize(){
-        // s.close();
     }
 }
